cca0da87179423f8fae42aed173de038fa775d42
Ivan <101908819+navirobayo@users.noreply.github.com>
2023-08-19 00:35:01 -0500

    Add files via upload

---
+++ Flutter/customizable_theme/home_widget.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import '../../components/questions.dart';
+import '../question_screen02/question_screen.dart';
+
+class HomeWidget extends StatefulWidget {
+  const HomeWidget({Key? key}) : super(key: key);
+
+  @override
+  State<HomeWidget> createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Trivia App 1.3"),
+      ),
+      body: ListView.builder(
+        itemCount: questionlist.length,
+        itemBuilder: (BuildContext context, int index) {
+          final card = questionlist[index];
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => QuestionScreen(
+                    title: card.title,
+                    hint1: card.hint1,
+                    hint2: card.hint2,
+                    question: card.question,
+                    solution: card.solution,
+                  ),
+                ),
+              );
+            },
+            child: Container(
+              padding: const EdgeInsets.all(20.0),
+              margin: const EdgeInsets.symmetric(vertical: 15, horizontal: 5),
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: Column(
+                children: [
+                  Text(
+                    card.title,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
