bbe8a7900e1ee93bf853d7c682a741d6c687f110
Ivan <101908819+navirobayo@users.noreply.github.com>
2023-08-19 00:43:44 -0500

    Add files via upload

---
+++ C/basic_variables.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include <stdbool.h>
+
+int main() {
+
+    int testOfInt = 24;  // %d
+    char testOfChar = 'a'; // %c
+    char testOfString[] = "What is happening bro?"; // %s
+    float testOfFloat = 3.14; // %f - 4 Bite
+    double testOfDouble = 3.14159265358979323846; // %lf - 8 Bite    
+    bool testOfBool = true; 
+    
+    int testOfVariable = 24;
+    
+    printf("This is a test on how you first"
+    "declare the position of the variable here: %d"
+    "and then you can call it again here: %d"
+    "remember to declare the variable again"
+    "if used multiple times, at the end.",
+     testOfVariable, testOfVariable);
+
+
+    return 0;
+}
